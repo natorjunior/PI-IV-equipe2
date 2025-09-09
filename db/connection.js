8d21eb30d6b9ba0bfb97c6dcb60c656cba4d36eb
Matheus Pereira <mtspereira.pro@gmail.com>
2025-09-09 13:55:12 -0300

    correção das propriedades da db para estabelecer conexão com o docker-compose

--- db/connection.js
+++ db/connection.js
@@ -4,9 +4,10 @@ const mysql = require("mysql2");
 //  Estabelecendo conexão com a base de dados
 const db = mysql.createConnection({
   host: "localhost",
-  user: "root",
-  password: "admin",
-  database: "pi_iv_equipe2",
+  port: 3308,
+  user: "app_user",
+  password: "app_pass",
+  database: "app_db",
 });
 
 //  Feedback da conexão
